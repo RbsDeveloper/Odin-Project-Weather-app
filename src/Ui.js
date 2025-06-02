83c354e696f19be271d3ef5613eb31312c112844
Raul <developer.rbs@gmail.com>
2025-06-02 23:53:24 +0300

    Completed base UI layout function

--- src/Ui.js
+++ src/Ui.js
@@ -32,8 +32,62 @@ export const createUi = (() => {
 
     const rightContainer = createEl('div', ['rightContainer'], '', {id: 'rightContainer'});
     
-    
-    main.append(leftContainer);
+    const nav = createEl('div', ['navigation',]);
+    const viewToggle = createEl('div', ['viewToggle']);
+    const todayBtn = createEl('button', ['todayBtn'], 'Today', {'data-view':'today', id:'todayBtn'});
+    const weekBtn = createEl('button', ['weekBtn'], 'Week', {'data-view':'week', id:'weekBtn'});
+    viewToggle.append(todayBtn, weekBtn);
+
+    const temperatureToggle = createEl('div', ['temperatureToggle']);
+    const celsiusUnit = createEl('button', ['celsiusBtn'], '°C', {'data-view':'celsius', id:'celsiusBtn'});
+    const fahrenheitUnit = createEl('button', ['fahrenheitBtn'], '°F', {'data-view': 'fahrenheit', id: 'fahrenheit'});
+    temperatureToggle.append(celsiusUnit, fahrenheitUnit);
+    nav.append(viewToggle, temperatureToggle);
+
+    const forecastDisplay = createEl('div', ['forecastContainer'], '', {id:'forecastDisplay'});
+
+    const highlightsContainer = createEl('div', ['highlightsContainer'], '', {id: 'highlightsContainer'});
+    const hlTitle = createEl('h3', ['hlTitle'], "Today's Highlights");
+
+    //Uv card
+    const uvCard = createEl('div', ['uvCard'], '', {id:'uvCard'});
+    const uvTitle = createEl('h4', ['hlCardTitle'], 'UV Index');
+    const chart = createEl('div', ['uvChart'], '', {id:'uvChart'});
+    uvCard.append(uvTitle, chart);
+    //Wind status
+    const windCard = createEl('div', ['windCard'], '', {id:'windCard'});
+    const windTitle = createEl('h4', ['hlCardTitle'], 'Wind Status');
+    const windValue = createEl('p', ['windVal', 'hhValue'], '', {id: 'windVal'});
+    const direction = createEl('p', ['windDirection'], '', {id: 'windDirection'});
+    windCard.append(windTitle, windValue, direction);
+    //Sunrise&Sunset
+    const ssCard = createEl('div', ['ssCard'],)
+    const ssTitle = createEl('h4', ['hlCardTitle'], 'Sunrise & Sunset');
+    ssCard.append(ssTitle);
+    //Humidity
+    const humidityCard = createEl('div', ['humidityCard'], '', {id:'humidityCard'});
+    const humidityTitle = createEl('h4', ['hlCardTitle'], 'Humidity');
+    const humidityValue = createEl('p', ['humidityVal', 'hhValue'], '', {id: 'humidityVal'});
+    const humidityState = createEl('p', ['humidityState', 'hhState'], '', {id: 'hhState'});
+    humidityCard.append(humidityTitle, humidityValue, humidityState);
+    //Visibility
+    const visibilityCard = createEl('div', ['visibilityCard'], '', {id:'visibilityCard'});
+    const visibilityTitle = createEl('h4', ['hlCardTitle'], 'Visibility');
+    const visibilityValue = createEl('p', ['visibilityVal', 'hhValue'], '', {id: 'visibilityVal'});
+    const visibilityState = createEl('p', ['visibilityState', 'hhState'], '', {id: 'hhState'});
+    visibilityCard.append(visibilityTitle, visibilityValue, visibilityState);
+    //Air Quality
+    const airQualityCard = createEl('div', ['airQualityCard'], '', {id:'airQualityCard'});
+    const airQualityTitle = createEl('h4', ['hlCardTitle'], 'Air Quality');
+    const airQualityValue = createEl('p', ['airQualityVal', 'hhValue'], '', {id: 'airQualityVal'});
+    const airQualityState = createEl('p', ['airQualityState', 'hhState'], '', {id: 'hhState'});
+    airQualityCard.append(airQualityTitle, airQualityValue, airQualityState);
+
+    highlightsContainer.append(hlTitle, uvCard, windCard, ssCard, humidityCard, visibilityCard, airQualityCard);
+    rightContainer.append(nav, forecastDisplay, highlightsContainer);
+
+
+    main.append(leftContainer, rightContainer);
     document.querySelector('body').append(main)
 
    
